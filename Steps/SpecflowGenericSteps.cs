9b87b6aa1b3f966b41404a245385a53bd3017e14
maucasas <33932244+maucasas@users.noreply.github.com>
2021-01-05 14:04:03 -0500

    Refactor in Project Automation

--- Steps/SpecflowGenericSteps.cs
+++ Steps/SpecflowGenericSteps.cs
@@ -73,167 +73,167 @@ namespace MDM_Automation_SpecFlow.Steps
         }
         #endregion
 
-        #region SideNav
-        [Given(@"the SideNav has loaded")]
-        [When(@"the SideNav has loaded")]
-        public void WhenTheSideNavHasLoaded()
-        {
-            SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
-            bool isPresent = SNavModule.IsSideNavDisplayed();
+        //#region SideNav
+        //[Given(@"the SideNav has loaded")]
+        //[When(@"the SideNav has loaded")]
+        //public void WhenTheSideNavHasLoaded()
+        //{
+        //    SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
+        //    bool isPresent = SNavModule.IsSideNavDisplayed();
 
-            Assert.AreEqual(true, isPresent);
-        }
+        //    Assert.AreEqual(true, isPresent);
+        //}
 
-        [Then(@"the Campaign LinkText should be (.*)")]
-        public void ThenTheCampaingLinkTextShouldBe(string isVisible)
-        {
+        //[Then(@"the Campaign LinkText should be (.*)")]
+        //public void ThenTheCampaingLinkTextShouldBe(string isVisible)
+        //{
 
-            //SpecflowGenericSteps sgs = new SpecflowGenericSteps();
-            SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
-            bool isPresent = SNavModule.IsCampaignLinkTextDisplayed();
+        //    //SpecflowGenericSteps sgs = new SpecflowGenericSteps();
+        //    SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
+        //    bool isPresent = SNavModule.IsCampaignLinkTextDisplayed();
 
-            switch (isVisible)
-            {
-                case "visible":
-                    Assert.AreEqual(true, isPresent);
-                    break;
-                case "not-visible":
-                    Assert.AreEqual(false, isPresent);
-                    break;
-            }
-        }
+        //    switch (isVisible)
+        //    {
+        //        case "visible":
+        //            Assert.AreEqual(true, isPresent);
+        //            break;
+        //        case "not-visible":
+        //            Assert.AreEqual(false, isPresent);
+        //            break;
+        //    }
+        //}
 
-        [Then(@"the Creative LinkText should be visible")]
-        public void ThenTheCreativeLinkTextShouldBeVisible()
-        {
-            SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
+        //[Then(@"the Creative LinkText should be visible")]
+        //public void ThenTheCreativeLinkTextShouldBeVisible()
+        //{
+        //    SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
 
-            bool IsVisible = SNavModule.IsCreativeContainerDisplayed();
+        //    bool IsVisible = SNavModule.IsCreativeContainerDisplayed();
 
-            Assert.AreEqual(true, IsVisible);
-        }
+        //    Assert.AreEqual(true, IsVisible);
+        //}
 
-        [Then(@"the Config LinkText should be (.*)")]
-        public void ThenTheConfigLinkTextShouldBe(string isVisible)
-        {
-            SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
-            bool isPresent = SNavModule.IsConfigContainerDisplayed();
+        //[Then(@"the Config LinkText should be (.*)")]
+        //public void ThenTheConfigLinkTextShouldBe(string isVisible)
+        //{
+        //    SideNavModule SNavModule = new SideNavModule(VariablesTest.WebDriver);
+        //    bool isPresent = SNavModule.IsConfigContainerDisplayed();
 
-            switch (isVisible)
-            {
-                case "visible":
-                    Assert.AreEqual(true, isPresent);
-                    break;
-                case "not-visible":
-                    Assert.AreEqual(false, isPresent);
-                    break;
-            }
-        }
+        //    switch (isVisible)
+        //    {
+        //        case "visible":
+        //            Assert.AreEqual(true, isPresent);
+        //            break;
+        //        case "not-visible":
+        //            Assert.AreEqual(false, isPresent);
+        //            break;
+        //    }
+        //}
 
-        private void AssertAreEqual(string expected, string actual)
-        {
-            try
-            {
-                Assert.AreEqual(expected, actual);
-            }
-            catch (Exception ex)
-            {
+        //private void AssertAreEqual(string expected, string actual)
+        //{
+        //    try
+        //    {
+        //        Assert.AreEqual(expected, actual);
+        //    }
+        //    catch (Exception ex)
+        //    {
 
-            }
+        //    }
 
-        }
+        //}
 
-        #endregion
+        //#endregion
 
-        #region ACRC_Actions
+        //#region ACRC_Actions
 
-        [Given(@"that the test user has selected '(.*)' agency")]
-        public void GivenThatTheTestUserHasSelectedAgency(string p0)
-        {
-            ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
-            clDataModObj.EnterTextInAgencyDropdown(p0);
-        }
+        //[Given(@"that the test user has selected '(.*)' agency")]
+        //public void GivenThatTheTestUserHasSelectedAgency(string p0)
+        //{
+        //    ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
+        //    clDataModObj.EnterTextInAgencyDropdown(p0);
+        //}
 
-        [Given(@"that the test user has selected '(.*)' client")]
-        public void GivenThatTheTestUserHasSelectedClient(string p0)
-        {
-            ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
-            clDataModObj.EnterTextInClientDropdown(p0);
-        }
+        //[Given(@"that the test user has selected '(.*)' client")]
+        //public void GivenThatTheTestUserHasSelectedClient(string p0)
+        //{
+        //    ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
+        //    clDataModObj.EnterTextInClientDropdown(p0);
+        //}
 
-        [Given(@"that the test user has selected '(.*)' region")]
-        public void GivenThatTheTestUserHasSelectedRegion(string p0)
-        {
-            ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
-            clDataModObj.EnterTextInRegionDropdown(p0);
-        }
+        //[Given(@"that the test user has selected '(.*)' region")]
+        //public void GivenThatTheTestUserHasSelectedRegion(string p0)
+        //{
+        //    ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
+        //    clDataModObj.EnterTextInRegionDropdown(p0);
+        //}
 
-        [Given(@"that the test user has selected '(.*)' country")]
-        public void GivenThatTheTestUserHasSelectedCountry(string p0)
-        {
-            ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
-            clDataModObj.EnterTextInCountryDropdown(p0);
-        }
+        //[Given(@"that the test user has selected '(.*)' country")]
+        //public void GivenThatTheTestUserHasSelectedCountry(string p0)
+        //{
+        //    ClientDataModule clDataModObj = new ClientDataModule(VariablesTest.WebDriver);
+        //    clDataModObj.EnterTextInCountryDropdown(p0);
+        //}
 
-        #endregion
+        //#endregion
 
-        #region CampaignActions
-        [Given(@"that the test user has selected a campaign")]
-        public void GivenThatTheTestUserHasSelectedACampaign()
-        {
-            CampaignsGridModule camModObj =new  CampaignsGridModule(VariablesTest.WebDriver);
-            camModObj.ClickInCampaignRowByIndex(1);
-        }
-        #endregion
+        //#region CampaignActions
+        //[Given(@"that the test user has selected a campaign")]
+        //public void GivenThatTheTestUserHasSelectedACampaign()
+        //{
+        //    CampaignsGridModule camModObj =new  CampaignsGridModule(VariablesTest.WebDriver);
+        //    camModObj.ClickInCampaignRowByIndex(1);
+        //}
+        //#endregion
 
-        #region CreativeActions
-        [Given(@"that the test user has selected new creative button")]
-        public void GivenThatTheTestUserHasSelectedNewCreativeButton()
-        {
-            CreativeHeaderActionsModule cHActionModObj = new CreativeHeaderActionsModule(VariablesTest.WebDriver);
-            cHActionModObj.ClickOnNewCreativeButton();
-        }
+        //#region CreativeActions
+        //[Given(@"that the test user has selected new creative button")]
+        //public void GivenThatTheTestUserHasSelectedNewCreativeButton()
+        //{
+        //    CreativeHeaderActionsModule cHActionModObj = new CreativeHeaderActionsModule(VariablesTest.WebDriver);
+        //    cHActionModObj.ClickOnNewCreativeButton();
+        //}
 
-        [Given(@"that the test user has selects the dropdowns options")]
-        public void GivenThatTheTestUserHasSelectsTheDropdownsOptions()
-        {
-            CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
-            crTXModObj.SelectOptionInDropdownsByIndex(1);
-        }
+        //[Given(@"that the test user has selects the dropdowns options")]
+        //public void GivenThatTheTestUserHasSelectsTheDropdownsOptions()
+        //{
+        //    CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
+        //    crTXModObj.SelectOptionInDropdownsByIndex(1);
+        //}
 
-        [Given(@"that the test user has populate freeforms text")]
-        public void GivenThatTheTestUserHasPopulateFreeformsText()
-        {
-            CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
-            crTXModObj.EnterTextInFreeFormsList("Free form loaded Automated");
-        }
+        //[Given(@"that the test user has populate freeforms text")]
+        //public void GivenThatTheTestUserHasPopulateFreeformsText()
+        //{
+        //    CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
+        //    crTXModObj.EnterTextInFreeFormsList("Free form loaded Automated");
+        //}
 
-        [Given(@"that the test user has selected datepicker")]
-        public void GivenThatTheTestUserHasSelectedDatepicker()
-        {
-            CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
-            crTXModObj.SelectStartDate(1);
-            crTXModObj.SelectEndDate(28);
-        }
+        //[Given(@"that the test user has selected datepicker")]
+        //public void GivenThatTheTestUserHasSelectedDatepicker()
+        //{
+        //    CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
+        //    crTXModObj.SelectStartDate(1);
+        //    crTXModObj.SelectEndDate(28);
+        //}
 
-        [When(@"the test user clicks on save button")]
-        public void WhenTheTestUserClicksOnSaveButton()
-        {
-            CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
-            crTXModObj.ClickOnSaveButton();
-        }
+        //[When(@"the test user clicks on save button")]
+        //public void WhenTheTestUserClicksOnSaveButton()
+        //{
+        //    CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
+        //    crTXModObj.ClickOnSaveButton();
+        //}
 
-        [Then(@"the creative taxonomy string is displayed")]
-        public void ThenTheCreativeTaxonomyStringIsDisplayed()
-        {
-            CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
-            bool isVisible = crTXModObj.IsVisibleTaxonomyString();
+        //[Then(@"the creative taxonomy string is displayed")]
+        //public void ThenTheCreativeTaxonomyStringIsDisplayed()
+        //{
+        //    CreativesTXModule crTXModObj = new CreativesTXModule(VariablesTest.WebDriver);
+        //    bool isVisible = crTXModObj.IsVisibleTaxonomyString();
 
-            Assert.IsTrue(isVisible);
-        }
+        //    Assert.IsTrue(isVisible);
+        //}
 
 
 
-        #endregion
+        //#endregion
     }
 }
